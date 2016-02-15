6e8263d6269781fe1f8b2cda3dcf3b31287fb349
Sean McNeil <sean@seanmcneil.com>
2016-02-14 23:41:49 -0800

    Initial commit

---
+++ Example/SMDynamicIcons/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SMDynamicIcons
+//
+//  Created by Sean McNeil on 02/14/2016.
+//  Copyright (c) 2016 Sean McNeil. All rights reserved.
+//
+
+@import UIKit;
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
